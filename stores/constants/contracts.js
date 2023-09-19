6b5c108657c476313de474d351f939e9e153807a
SandySwap <hello@sandyswap.xyz>
2023-09-19 19:48:20 +0300

    updated contracts

--- stores/constants/contracts.js
+++ stores/constants/contracts.js
@@ -1,13 +1,13 @@
 import abis from "../abis";
 
-export const GOV_TOKEN_ADDRESS = "0x888EF71766ca594DED1F0FA3AE64eD2941740A20";
+export const GOV_TOKEN_ADDRESS = "0xea11ffe430716d48ff749d04cffbddf7c2618f0d";
 export const GOV_TOKEN_NAME = "Sandy";
 export const GOV_TOKEN_SYMBOL = "SANDY";
 export const GOV_TOKEN_DECIMALS = 18;
 export const GOV_TOKEN_LOGO = "https://prod.solidly.exchange/Sandy Swap-O.svg";
 export const GOV_TOKEN_ABI = abis.tokenABI;
 
-export const VE_TOKEN_ADDRESS = "0xcBd8fEa77c2452255f59743f55A3Ea9d83b3c72b";
+export const VE_TOKEN_ADDRESS = "0xF7140bf00E5Dca2bc1117B2e088A3105f5515F18";
 export const VE_TOKEN_NAME = "veSANDY";
 export const VE_TOKEN_SYMBOL = "veSANDY";
 export const VE_TOKEN_DECIMALS = 18;
@@ -27,16 +27,16 @@ export const ETH_DECIMALS = 18;
 export const ETH_LOGO =
   "https://assets.coingecko.com/coins/images/4001/large/ethereum.png";
 
-export const FACTORY_ADDRESS = "0x3fAaB499b519fdC5819e3D7ed0C26111904cbc28";
+export const FACTORY_ADDRESS = "0xdb9908b6e0b87338194ae8627583194994bd992d";
 export const FACTORY_ABI = abis.factoryABI;
 
-export const ROUTER_ADDRESS = "0xa38cd27185a464914D3046f0AB9d43356B34829D";
+export const ROUTER_ADDRESS = "0x6271ebf79b8f8a92b13bc41901b7565dea218f4d";
 export const ROUTER_ABI = abis.routerABI;
 
-export const VE_DIST_ADDRESS = "0xA5CEfAC8966452a78d6692837b2ba83d19b57d07";
+export const VE_DIST_ADDRESS = "0xfd96d121ddc5aaf1ca77f99411190e4818a79884";
 export const VE_DIST_ABI = abis.veDistABI;
 
-export const VOTER_ADDRESS = "0xdC819F5d05a6859D2faCbB4A44E5aB105762dbaE";
+export const VOTER_ADDRESS = "0x975cccf9198def59c9d745b4d8ae1ea6bdd1b3ea";
 export const VOTER_ABI = abis.voterABI;
 
 export const ERC20_ABI = abis.erc20ABI;
@@ -45,4 +45,4 @@ export const GAUGE_ABI = abis.gaugeABI;
 export const BRIBE_ABI = abis.bribeABI;
 export const TOKEN_ABI = abis.tokenABI;
 
-export const MULTICALL_ADDRESS = "0x11473d6e641df17cd6331d45b135e35b49edbea8";
+export const MULTICALL_ADDRESS = "0xf10189d6b187bd71475864979b1ebbe203f63183";
